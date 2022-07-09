54d09b666d25e4a34e443afefc8a28d15092ccb3
mtrice <mdtrice@gmail.com>
2022-07-09 11:20:40 -0600

    inital check all links spec

--- cypress/e2e/links.spec.ts
+++ cypress/e2e/links.spec.ts
@@ -1,9 +1,63 @@
 /// <reference types="cypress" />
 
-context('Validate links', () => {
+const { _ } = Cypress
 
-    it('Should load page', ()=>{
-     cy.visit('/standards/badpage', {failOnStatusCode:false})
+
+describe('test w3', () => {
+
+    it('can visit all badpage links', () => {
+        cy.visit('https://www.w3.org/standards/badpage', { failOnStatusCode: false })
+        cy.get("a:not([href*='mailto:'])")
+            .each($a => {
+                if(!$a.prop('href')) return   
+                cy.request(
+                    {
+                        url: $a.prop('href'),
+                        failOnStatusCode: false
+                    }).then($res => {
+                        it(`can visit ${$a.prop('href')}`, () => {
+
+                            expect($res.isOkStatusCode).to.eq(200)
+                        })
+                    })
+            })
     })
 
+    it('can visit all multimodal links', () => {
+        cy.visit('https://www.w3.org/standards/webofdevices/multimodal')
+        cy.get("a:not([href*='mailto:'])")
+            .each($a => {
+             if(!$a.prop('href')) return                
+             cy.request(
+                    {
+                        url: $a.prop('href'),
+                        failOnStatusCode: false
+                    }).then($res => {
+                        it(`can visit ${$a.prop('href')}`, () => {
+
+                            expect($res.isOkStatusCode).to.eq(200)
+                        })
+                    })
+            })
+    })
+
+    it('can visit all htmlcss links', () => {
+        cy.visit('https://www.w3.org/standards/webdesign/htmlcss')
+        cy.get("a:not([href*='mailto:'])")
+            .each($a => {
+                if(!$a.prop('href')) return   
+                cy.request(
+                    {
+                        url: $a.prop('href'),
+                        failOnStatusCode: false
+                    }).then($res => {
+                        it(`can visit ${$a.prop('href')}`, () => {
+
+                            expect($res.isOkStatusCode).to.eq(200)
+                        })
+                    })
+            })
+
+
+    })
 })
