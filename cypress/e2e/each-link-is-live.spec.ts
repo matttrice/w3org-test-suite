1716c33eeb732e8da9889d9e953002a7873caae7
mtrice <mdtrice@gmail.com>
2022-07-10 00:45:36 -0600

    add page visit for 200 response via requirements

--- cypress/e2e/each-link-is-live.spec.ts
+++ cypress/e2e/each-link-is-live.spec.ts
@@ -1,16 +1,19 @@
 import 'cypress-each'
-import { apiLinks } from '@fixtures/config/type/apiLinks'
+import { apiLinks } from '@fixtures/type/apiLinks'
 
 // use preprocessed links with cypress-each  
 // to create a separate test for each selector
 const pages: Array<apiLinks> = Cypress.env('links')
+const page1 = pages[0]
+context(`${page1.name} @ ${page1.url}`, () => {
+    it('can visit page (i.e. 200)', () => {
+        cy.visit(page1.url)
+    })
 
-context(`${pages[0].name} @ ${pages[0].url}`, () => {
-    
-    it.each(pages[0].links)(`${pages[0].name} %s is live`, (page) => {
-        cy.request({ method:'GET', url:page, failOnStatusCode: false })
-        .then(res =>{
-            expect(res.status).to.not.be.within(400,599,'Error Code Found')
-        })
+    it.each(page1.links)(`${page1.name} %s is live`, (preproccedLink) => {
+        cy.request({ method: 'GET', url: preproccedLink, failOnStatusCode: false })
+            .then(res => {
+                expect(res.status).to.not.be.within(400, 599, 'Error Code Found')
+            })
     })
 })
