909d026c48822674fa9f0dbf84ac1c98afa89326
mtrice <mdtrice@gmail.com>
2022-07-09 22:59:46 -0600

    match requirements for linked pages

--- cypress/e2e/each-link-is-live.spec.ts
+++ cypress/e2e/each-link-is-live.spec.ts
@@ -1,25 +1,16 @@
 import 'cypress-each'
 import { apiLinks } from '@fixtures/config/type/apiLinks'
 
-// use preprocessed links with cypress-each 
+// use preprocessed links with cypress-each  
 // to create a separate test for each selector
 const pages: Array<apiLinks> = Cypress.env('links')
 
 context(`${pages[0].name} @ ${pages[0].url}`, () => {
-    it.each(pages[0].links)('Link %s is live', (page) => {
-        cy.visit(page, { failOnStatusCode: false })
-        cy.get("a:not([href*='mailto:'])")
-            .each($a => {
-                it(`can visit ${$a.prop('href')}`, () => {
-                    if (!$a.prop('href')) return
-                    cy.request(
-                        {
-                            url: $a.prop('href'),
-                            failOnStatusCode: false
-                        }).then($res => {
-                            expect($res.isOkStatusCode).to.eq(200)
-                        })
-                })
-            })
+    
+    it.each(pages[0].links)(`${pages[0].name} %s is live`, (page) => {
+        cy.request({ method:'GET', url:page, failOnStatusCode: false })
+        .then(res =>{
+            expect(res.status).to.not.be.within(400,599,'Error Code Found')
+        })
     })
 })
