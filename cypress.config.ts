379d21de24aa8c3ef288b4ef0fdbcf5d7e9fad32
mtrice <mdtrice@gmail.com>
2022-07-10 01:53:59 -0600

    install and configure cucumber

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,21 +2,35 @@ import { defineConfig } from 'cypress'
 import { pages } from './cypress/fixtures/scrapePages'
 import { getApiLinks } from './util/getApiLinks'
 
-export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
-  e2e: {
-    async setupNodeEvents(on, config) {
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import browserify from '@badeball/cypress-cucumber-preprocessor/browserify';
 
-      // dynamically preprocess list of urls to 
-      // use with cypress-each plugin
-      config.env.links = await getApiLinks(pages)
-      
-      return config
-    },
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config)
+  on(
+    'file:preprocessor',
+    browserify(config, {
+      typescript: require.resolve('typescript'),
+    })
+  )
+
+  // getApiLinks pre-processes the list of anchors links 
+  // the spec uses with cypress-each plugin
+  config.env.links = await getApiLinks(pages)
+
+  return config
+}
+
+export default defineConfig({
+  e2e: {
     baseUrl: 'https://www.w3.org',
-    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
-  }
+    specPattern: ['cypress/e2e/**/*.spec.{js,jsx,ts,tsx}', '**/*.feature'],
+    supportFile: false,
+    setupNodeEvents,
+  },
 })
 
 
