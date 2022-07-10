2de6f9446e30c46b6779fb53e210283bf229fb8b
mtrice <mdtrice@gmail.com>
2022-07-10 16:41:46 -0600

    tidy console error detection, gitignore, package

--- cypress/e2e/spy-console-works.spec.ts
+++ cypress/e2e/spy-console-works.spec.ts
@@ -1,28 +1,31 @@
 /**
- * Determin if console errors are detected by 
+ * Determine if console errors are detected by 
  * loading static html with known console error
  */
-context('Validate method of detecting errors', {
+context('Validate approach to detecting console errors', {
        baseUrl : null } ,() => {
     
-    it('Console error detected with spy', () => {
+    it('Should detect error with spy', () => {
         cy.visit('cypress/fixtures/other/console-error.html', {
             onBeforeLoad(win) {
-                cy.spy(win.console, 'error').as('errorLog')       
+                cy.spy(win.console, 'error').as('spyErrorLog')       
             },
         })
         cy.window().then((win) => {
-            expect(win.console.error).to.have.callCount(1);
+            cy.get('@spyErrorLog').should('be.calledOnce')
+            cy.get('@spyErrorLog').should('be.calledWith', 'Hello Satoshi!')
         })
     })
 
-    it('Console error detected with stub and alias', () => {
+    it('Should detect error with stub', () => {
         cy.visit('cypress/fixtures/other/console-error.html', {
             onBeforeLoad(win) {
-                cy.stub(win.console, 'error').as('errorLog')
+                cy.stub(win.console, 'error').as('stubErrorLog')
             },
         })
-        cy.get('@errorLog').should('be.calledWith', 'Hello World!')
-
+        cy.get('@stubErrorLog').should('be.calledWith', 'Hello Satoshi!')
+        cy.window().then((win) => {
+            expect(win.console.error).to.have.callCount(1)
+        })
     })
 })
