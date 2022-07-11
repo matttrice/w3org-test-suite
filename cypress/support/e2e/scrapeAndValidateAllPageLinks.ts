c54ab47aa56b3e5a5baf41a6758895c0fe1d756f
mtrice <mdtrice@gmail.com>
2022-07-10 22:38:05 -0600

    log count of found links

--- cypress/support/e2e/scrapeAndValidateAllPageLinks.ts
+++ cypress/support/e2e/scrapeAndValidateAllPageLinks.ts
@@ -1,5 +1,7 @@
 Cypress.Commands.add('scrapeAndValidateAllPageLinks', () => {
-    cy.get("a:not([href*='mailto:'])")
+    cy.get("a:not([href*='mailto:'])").then($a =>{
+        cy.log(`found ${$a.length}`)
+        })
         .each($a => {
             if (!$a.prop('href'))
                 return;
