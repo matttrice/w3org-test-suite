661d4df4eabe5bdd30121bb918774c2545bfd059
mtrice <mdtrice@gmail.com>
2022-07-11 19:46:12 -0600

    add comments

--- cypress/support/e2e/validatePageLink.ts
+++ cypress/support/e2e/validatePageLink.ts
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+/**
+ * Send rest call to validate the page link is live
+ */
 Cypress.Commands.add('validatePageLink', (link: string) => {
     cy.request({ method: 'GET', url: link, failOnStatusCode: false })
         .then(res => {
--- cypress/support/e2e/validatePageLoad.ts
+++ cypress/support/e2e/validatePageLoad.ts
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
 import { scrapePage } from '@fixtures/type/scrapePage'
-
+/**
+ * Vist page but allow 404s to be caught and asserted by the intercept
+ */
 Cypress.Commands.add('validatePageLoad', (page: scrapePage) => {
     cy.intercept(page.url).as(`${page.name}`)
-    // vist page and allow 404s to be caught and asserted by the intercept
     cy.visit(page.url, { failOnStatusCode: false })
     cy.wait(`@${page.name}`).its('response.statusCode').should('eq', 200)
 });
