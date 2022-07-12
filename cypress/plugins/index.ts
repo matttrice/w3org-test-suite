eef9a64a96875f84cc59c86b07bae955bb566975
mtrice <mdtrice@gmail.com>
2022-07-11 22:02:46 -0600

    downgrade cypress to 9.7

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,29 @@
+import { pages } from '../fixtures/pagesToScrape'
+import { getApiLinks } from '../../util/getApiLinks'
+
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
+import browserify from '@badeball/cypress-cucumber-preprocessor/browserify'
+
+/**
+ * 
+ * @type {Cypress.PluginConfig}
+ */
+
+module.exports = async (on, config)=>{
+    await addCucumberPreprocessorPlugin(on, config)
+    on(
+      'file:preprocessor',
+      browserify(config, {
+        typescript: require.resolve('typescript'),
+      })
+    )
+      // getApiLinks pre-processes the list of anchors links 
+  // the spec uses with cypress-each plugin
+  config.env.links = await getApiLinks(pages)
+
+  return config
+} 
+
+
+
+
